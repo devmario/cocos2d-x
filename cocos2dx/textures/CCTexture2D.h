cad0965f5dc51f452d40050cf588ca4c01f80dc0
devmario <mario@vanillabreeze.com>
2013-02-08 18:43:53 +0900

    tex

--- cocos2dx/textures/CCTexture2D.h
+++ cocos2dx/textures/CCTexture2D.h
@@ -262,6 +262,7 @@ private:
     /** texture max T */
     CC_PROPERTY(GLfloat, m_fMaxT, MaxT)
     /** content size */
+	
     CC_PROPERTY_READONLY(CCSize, m_tContentSize, ContentSize)
 
     /** whether or not the texture has their Alpha premultiplied */
